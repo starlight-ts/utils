4cabd448b0cd03227f48845fa7904d720778dc6a
Gryffon Bellish <owenbellish@gmail.com>
2020-07-29 20:01:39 -0400

    docs

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,3 +1,7 @@
 declare module '@pyrotechniac/utils' {
+	/**
+	 * Reads a file and returns a buffer.
+	 * @param path The path to the file.
+	 */
 	export function readFile(path: string): Promise<Buffer>;
 }
