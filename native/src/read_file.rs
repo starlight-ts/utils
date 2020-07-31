6cddbf980ee920a2cb4485134186eeee5e502636
Gryffon Bellish <owenbellish@gmail.com>
2020-07-31 18:46:36 -0400

    src: match instead of if let and [publish binaries]

--- native/src/read_file.rs
+++ native/src/read_file.rs
@@ -29,10 +29,10 @@ impl Task for FileReaderTask {
         mut cx: TaskContext,
         result: Result<Self::Output, Self::Error>,
     ) -> JsResult<Self::JsEvent> {
-        if let Err(error) = result {
-            return cx.throw_type_error(format!("{}", error));
-        };
-        let filebytes = result.unwrap();
+		let filebytes = match result {
+			Ok(file) => file,
+			Err(err) => return cx.throw_type_error(format!("{}", err))
+		};
         let buffer = cx.buffer(filebytes.len() as u32)?;
         for (i, byte) in filebytes.iter().enumerate() {
             let js_byte = cx.number(*byte);
