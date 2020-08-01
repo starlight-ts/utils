e1d1b578a1e2a26eeab4099b0b190a5486cec8bd
Gryffon Bellish <owenbellish@gmail.com>
2020-08-01 17:35:59 -0400

    fix indentation:

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -1,10 +1,10 @@
 use neon::register_module;
 use read_file::FileReaderTask;
 
-mod read_file;
 mod errors;
+mod read_file;
 
 register_module!(mut m, {
-	m.export_function("readFile", FileReaderTask::read_file)?;
+    m.export_function("readFile", FileReaderTask::read_file)?;
     Ok(())
 });
